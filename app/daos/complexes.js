92fcdbadbf372fffaa914e352818f437d94d7c34
Klaus Niedermair <klaus.niedermair007@gmail.com>
2018-08-23 15:19:14 +0200

    feat(server - complex return value): also return the comment and the proteins contained in the complex

--- app/daos/complexes.js
+++ app/daos/complexes.js
@@ -18,7 +18,7 @@ module.exports = function(context) {
         getComplexWhichHasProtein: function(uniprotId) {
             console.warn(`getComplexWhichHasProtein still uses sql query`);
             const query = `
-                select c.id, c.name
+                select c.id, c.name, c.comment, c.proteins
                 from complexes c, proteins p, protein_complexes cp
                 where c.id = cp."complexId" and p."uniprotId" = cp."uniprotId" and p."uniprotId" = :uniprotId;
             `;
