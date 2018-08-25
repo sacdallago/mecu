64a7688addc750e54fa113b7b92359c4f1d7b50f
Klaus Niedermair <klaus.niedermair007@gmail.com>
2018-08-25 11:15:43 +0200

    fix: log messages

--- frontend/js/requests.js
+++ frontend/js/requests.js
@@ -12,7 +12,7 @@ TemperatureService.temperatureReadsToProteinsAndExperimentPairs = (pairs) => {
         )
         .then(resp => resp.json())
         .catch(error => {
-            console.error('Request error for temperatureReadsToProteinsAndExperimentPairs: ', error, pairs);
+            console.error('Request error for TemperatureService.temperatureReadsToProteinsAndExperimentPairs: ', error, pairs);
             return [];
         });
 }
@@ -30,7 +30,7 @@ TemperatureService.temperatureReads = (experiments, proteins) => {
         })
         .then(res => res.json())
         .catch(error => {
-            console.error('Request error for temperatureReads: ', error, experiments, proteins);
+            console.error('Request error for TemperatureService.temperatureReads: ', error, experiments, proteins);
             return [];
         })
 }
@@ -40,7 +40,7 @@ ExperimentService.paginatedExperiments = (queryObj) => {
     return fetch('/api/experiments?'+$.param(queryObj)) // edge and IE do not support https://developer.mozilla.org/en-US/docs/Web/API/URL/searchParams , consider polyfills?
         .then(resp => resp.json())
         .catch(error => {
-            console.error('Request error for paginatedExperiments: ', error, queryObj);
+            console.error('Request error for ExperimentService.paginatedExperiments: ', error, queryObj);
             return [];
         });
 }
@@ -48,7 +48,7 @@ ExperimentService.experimentsWhichHaveProtein = (uniprotId) => {
     return fetch(`/api/experiments/${uniprotId}`)
         .then(resp => resp.json())
         .catch(error => {
-            console.error('Request error for experimentsWhichHaveProtein: ', error, queryObj);
+            console.error('Request error for ExperimentService.experimentsWhichHaveProtein: ', error, queryObj);
             return [];
         });
 }
@@ -58,7 +58,7 @@ ProteinService.getSpecificProtein = (uniprotId, experimendId) => {
     return fetch(`/api/protein/${uniprotId}/experiment/${experimendId}`)
         .then(resp => resp.json())
         .catch(error => {
-            console.error('Request error for getSpecificProtein: ', error, uniprotId, experimendId);
+            console.error('Request error for ProteinService.getSpecificProtein: ', error, uniprotId, experimendId);
             return [];
         });
 }
@@ -68,7 +68,7 @@ ComplexService.getComplexById = (id) => {
     return fetch(`/api/complex/${id}`)
     .then(resp => resp.json())
     .catch(error => {
-        console.error('Request error for getComplexById: ', error, uniprotId, experimendId);
+        console.error('Request error for ComplexService.getComplexById: ', error, uniprotId, experimendId);
         return [];
     });
 }
@@ -76,7 +76,7 @@ ComplexService.getAllComplexesWhichContainProtein = (uniprotId) => {
     return fetch(`/api/complex/hasprotein/${uniprotId}`)
     .then(resp => resp.json())
     .catch(error => {
-        console.error('Request error for getAllComplexesWhichContainProtein: ', error, uniprotId, experimendId);
+        console.error('Request error for ComplexService.getAllComplexesWhichContainProtein: ', error, uniprotId, experimendId);
         return [];
     });
 }
