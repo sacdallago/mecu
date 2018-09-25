5959a50d04432161e4b567fa2a6d6fa29a352c43
Klaus Niedermair <klaus.niedermair007@gmail.com>
2018-09-25 11:07:09 +0200

    feat(helper functions): add function to extract googleId from user request

---
+++ app/helper.js
@@ -0,0 +1,9 @@
+module.exports = {
+    retrieveUserGoogleId: (request) => {
+        if(request && request.user) {
+            return request.user.get('googleId');
+        } else {
+            return '';
+        }
+    }
+};
