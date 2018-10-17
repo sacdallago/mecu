7a976bb7c78f3442863c0fbb7a5309a22f250454
Klaus Niedermair <klaus.niedermair007@gmail.com>
2018-10-17 23:31:53 +0200

    feat(find complexes): implement /api/complex/find
    works with limit/order/search pagination etc.

--- app/daos/complexes.js
+++ app/daos/complexes.js
@@ -14,6 +14,61 @@ module.exports = (context) => {
             .then(([result, metadata]) => result || {});
         },
 
+        findComplex: ({limit, offset, sortBy, order, search}) => {
+
+            const name = search.name || '';
+            const proteinList = search.proteinList || [];
+
+            const replacements = {
+                name: '%'+name+'%',
+                limit: limit,
+                offset: offset,
+                sortBy: sortBy
+            };
+
+            let nameWhereQuery = '';
+            if(search.name.length !== 0) {
+                nameWhereQuery = ' and name like :name ';
+            }
+
+            let proteinWhereQuery = '';
+            if(proteinList && proteinList.length > 0) {
+                proteinWhereQuery = '  where ';
+                proteinList.forEach((p,i) => {
+                    proteinWhereQuery += ` "uniprotId" = :p${i} `;
+                    replacements['p'+i] = p;
+                    if(i !== proteinList.length - 1) {
+                        proteinWhereQuery += ' or ';
+                    }
+                })
+            }
+
+            const query = `
+                select
+                    id,
+                    name,
+                    proteins,
+                    count(*) over() as total
+                from
+                    complexes c,
+                    (select "complexId", count(*) from protein_complexes ${proteinWhereQuery} group by "complexId") sub
+                where
+                    sub."complexId" = c.id and
+                    sub.count >= ${proteinList.length || 1}
+                    ${nameWhereQuery}
+                order by ${sortBy} ${order}
+                offset :offset
+                limit :limit;
+            `;
+
+            return context.dbConnection.query(
+                    query,
+                    {replacements: replacements},
+                    {type: sequelize.QueryTypes.SELECT}
+                )
+                .then(([result, metadata]) => result);
+        },
+
         getComplexWhichHasProtein: (uniprotId) => {
             const query = `
                 select c.id, c.name, c.comment, c.proteins
