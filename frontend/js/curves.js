d0897c40773595612a7464a7c50a8ec79d947a85
Klaus Niedermair <klaus.niedermair007@gmail.com>
2018-10-26 11:42:14 +0200

    feat(curves ppi table): add helper file which contains the drawing of the table
    draw the table in the actual curves.js

--- frontend/js/curves.js
+++ frontend/js/curves.js
@@ -2,7 +2,7 @@ let experimentsToDraw = [];
 let proteinsToDraw = [];
 const AMOUNT_OF_PPI_TO_DRAW = 20;
 let ppiTableData = [];
-const MAX_ROW_COLS_PPI_TABLE = 12;
+const MAX_ROW_COLS_PPI_TABLE = 11;
 
 const proteinCurvesGridIdentifier = '.isoGrid';
 const proteinCurvesGrid = $(proteinCurvesGridIdentifier).isotope({
@@ -197,6 +197,7 @@ const drawExperimentsSelect = (experiments) => {
         onChange: (e) => {
             experimentsToDraw = e.split(',').map(e => parseInt(e));
             populateGlobalsGraphs(getColoringValue());
+            drawPPITable();
         }
     });
 }
@@ -219,129 +220,25 @@ const drawProteinsSelect = (proteins) => {
         onChange: (e) => {
             proteinsToDraw = e.split(',');
             populateGlobalsGraphs(getColoringValue());
+            drawPPITable();
         }
     });
 }
 
-const drawPPITable = (relativeCorrelation) => {
-
-    const thead = $('#ppi-thead');
-    thead.empty();
-    const tbody = $('#ppi-tbody');
-    tbody.empty();
-
-    const pPlusE = (obj, id) => obj[id]+'-'+obj[id+'_exp'];
-    const extractE = (obj) => {
-        return obj.split('-')[1];
-    }
-
-    const proteinSet = {};
-    ppiTableData.forEach(p => {
-        if(!(proteinSet[pPlusE(p,'interactor1')] || []).find(o =>
-                o.interactor1 === p.interactor1 &&
-                o.interactor2 === p.interactor2 &&
-                o.interactor1_exp === p.interactor1_exp &&
-                o.interactor2_exp === p.interactor2_exp
-            )
+const drawPPITable = () => {
+    const filteredData = ppiTableData.filter(obj => {
+        if(
+            (proteinsToDraw.indexOf(obj.interactor1) > -1 && experimentsToDraw.indexOf(obj.interactor1_experiment) > -1) &&
+            (proteinsToDraw.indexOf(obj.interactor2) > -1 && experimentsToDraw.indexOf(obj.interactor2_experiment) > -1)
         ) {
-            proteinSet[pPlusE(p,'interactor1')] ? proteinSet[pPlusE(p,'interactor1')].push(p) : proteinSet[pPlusE(p,'interactor1')] = [p];
+            return true;
         }
-        if(!(proteinSet[pPlusE(p,'interactor2')] || []).find(o =>
-                o.interactor1 === p.interactor1 &&
-                o.interactor2 === p.interactor2 &&
-                o.interactor1_exp === p.interactor1_exp &&
-                o.interactor2_exp === p.interactor2_exp
-            )
-        ) {
-            proteinSet[pPlusE(p,'interactor2')] ? proteinSet[pPlusE(p,'interactor2')].push(p) : proteinSet[pPlusE(p,'interactor2')] = [p];
-        }
-    });
-    let proteinArray = Object.keys(proteinSet)
-
-    // sort the table rows/columns by experiment
-    proteinArray.sort((a,b) => {
-        let tmp1 = a.split('-').map(t => t.trim());
-        let tmp2 = b.split('-').map(t => t.trim());
-        return parseInt(tmp1[1]) > parseInt(tmp2[1]);
+        return false;
     });
 
-    // only show MAX_ROW_COLS_PPI_TABLE
-    if(proteinArray.length > MAX_ROW_COLS_PPI_TABLE) {
-        proteinArray = proteinArray.slice(0,MAX_ROW_COLS_PPI_TABLE);
-    }
-
-    // popuplate header
-    const trhead = $('<tr />');
-    trhead.append($('<th />'));
-    trhead.append(
-        proteinArray.map(p =>
-            $('<th>')
-                .attr({'style': `background-color: ${HelperFunctions.stringToColor(extractE(p)+'000')}`})
-                .text(p)
-            )
-    );
-    thead.append(trhead);
-
-    // find out what's min and max of all correlations to be shown
-    const minMaxCorr = {min: Number.MAX_SAFE_INTEGER, max: Number.MIN_SAFE_INTEGER};
-    if(relativeCorrelation) {
-        proteinArray.forEach(p1 =>
-            proteinArray.forEach(p2 => {
-                proteinSet[p1].forEach(pTmp => {
-                    if(pTmp.correlation !== null && pTmp.correlation < minMaxCorr.min) minMaxCorr.min = pTmp.correlation;
-                    if(pTmp.correlation !== null && pTmp.correlation > minMaxCorr.max) minMaxCorr.max = pTmp.correlation;
-                })
-            })
-        )
-    }
-
-    proteinArray.forEach((p1,i) => {
-        const row = $('<tr />').append($('<td />').attr({'style': `background-color: ${HelperFunctions.stringToColor(extractE(p1)+'000')}`}).text(p1));
-        for(let empty = 0; empty < i; empty++) {
-            row.append($('<td />'));
-        }
-        proteinArray.slice(i,proteinArray.length).forEach(p2 => {
-            const tdata = $('<td />');
-            let d;
-                    d = proteinSet[p1].find(obj => {
-
-                        return (pPlusE(obj,'interactor1') === p1 && pPlusE(obj,'interactor2') === p2) ||
-                            (pPlusE(obj,'interactor1') === p2 && pPlusE(obj,'interactor2') === p1)
-                    });
-            if(d) {
-                tdata.append(
-                    $('<div />')
-                        .addClass('table-data-content')
-                        .append([
-                            $('<div />')
-                                .text(d.distance.toFixed(2))
-                                .addClass('distance-div')
-                                .attr({'style': `background-color: rgba(0,255,0, ${1-d.distance})`}),
-                            $('<div />')
-                                .addClass('correlation-div')
-                                .text(d.correlation ? d.correlation.toFixed(2) : '-')
-                                .attr({'style': `background-color: rgb(52, 152, 219, ${
-                                    relativeCorrelation && d.correlation ?
-                                        (d.correlation - minMaxCorr.min) / (minMaxCorr.max - minMaxCorr.min):
-                                        d.correlation || 0
-                                    })`}),
-                        ])
-                        .attr({
-                            'data-html':`(${d.interactor1} ${d.interactor1_exp}) <-> (${d.interactor2} ${d.interactor2_exp}):<br>
-                                Distance: ${d.distance.toFixed(5)}<br>
-                                Correlation: ${d.correlation ? d.correlation : 'no data'}`
-                        })
-                        .popup({position: 'bottom left'})
-                    );
-            }
-            row.append(tdata);
-        });
-
-        tbody.append(row);
-    })
-
-
+    FullscreenHelper.drawPPITable('ppi-thead', 'ppi-tbody', filteredData, true, MAX_ROW_COLS_PPI_TABLE);
 }
+
 
 $('#coloring-dropdown').dropdown({
     onChange: () => {
@@ -390,11 +287,11 @@ $('#fullscreen-button').on('click', function() {
 $('#relative-absolute-corr-button').on('click', function() {
     switch($('#relative-absolute-corr-button').text()) {
         case 'Relative correlation':
-            drawPPITable(true);
+            drawPPITable();
             $('#relative-absolute-corr-button').text('Absolute Correlation');
             break;
         case 'Absolute Correlation':
-            drawPPITable(false);
+            drawPPITable();
             $('#relative-absolute-corr-button').text('Relative correlation');
             break;
 
@@ -420,14 +317,13 @@ $(document).ready(() => {
     const ppiDistances = ProteinService.getProteinXProteinDistances(proteinList, experimentList)
         .then(result => {
             console.log('ppiDistances', result);
+            // IMPROVEMENT: only save the ones which should be drawn (limited by MAX_ROW_COLS_PPI_TABLE));
+            // IMPROVEMENT 2: only request the ones you want to draw!
             ppiTableData = result;
-            drawPPITable(true);
+            drawPPITable();
         })
 
     Promise.resolve()
         .then(() => populateGlobalsGraphs(getColoringValue(), []))
-        .then(() => loadProteins())
-        .then(() => {
-            return
-        });
+        .then(() => loadProteins());
 });
