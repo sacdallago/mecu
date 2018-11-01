c8e24bada16ab925496574aa5eef019689f1fa55
Klaus Niedermair <klaus.niedermair007@gmail.com>
2018-11-01 16:13:17 +0100

    fix(query): fix () in query

--- app/daos/temperatureReads.js
+++ app/daos/temperatureReads.js
@@ -58,6 +58,7 @@ module.exports = function(context) {
             };
             let whereClause = ``;
             if(query.search.constructor === Array) {
+                whereClause += `AND (`;
                 query.search.forEach((v,i,a) => {
                     if(i != 0) {
                         whereClause += ` or `;
@@ -66,8 +67,9 @@ module.exports = function(context) {
                     whereClause += `p."uniprotId" like :`+replStr;
                     replacements[replStr] = v;
                 });
+                whereClause += `)`;
             } else {
-                whereClause = `p."uniprotId" like :search`;
+                whereClause = `and (p."uniprotId" like :search)`;
             }
             const sqlQuery = `
                 SELECT tmp."uniprotId", json_agg(json_build_object('experiment', tmp.experiment, 'reads', tmp.reads)) AS experiments
@@ -79,8 +81,8 @@ module.exports = function(context) {
                         (e.private = :isPrivate or e.uploader = :uploader) AND
                         e_tr."temperatureReadId" = tr.id AND
                         p_tr."uniprotId" = p."uniprotId" AND
-                        p_tr."temperatureReadId" = tr.id AND
-                        (${whereClause})
+                        p_tr."temperatureReadId" = tr.id
+                        ${whereClause}
                     GROUP BY tr."experiment", tr."uniprotId"
                     ORDER BY "uniprotId" asc, experiment asc
                     OFFSET :offset
@@ -97,8 +99,8 @@ module.exports = function(context) {
                         (e.private = :isPrivate or e.uploader = :uploader) AND
                         e_tr."temperatureReadId" = tr.id AND
                         p_tr."uniprotId" = p."uniprotId" AND
-                        p_tr."temperatureReadId" = tr.id AND
-                        (${whereClause})
+                        p_tr."temperatureReadId" = tr.id
+                        ${whereClause}
                     GROUP BY tr."experiment", tr."uniprotId"
                 ) t;
             `;
