72660551d4675f2bff46ae498b6a017376aebdf8
Klaus Niedermair <klaus.niedermair@quickbirdstudios.com>
2019-02-11 20:09:13 +0100

    remove automatic tour; and saving of tour settings

--- frontend/js/about.js
+++
@@ -1,7 +0,0 @@
-$(`#reset-tour-button`).on(`click`, function() {
-    Object.keys(pageNames).forEach(k => {
-        StorageManager.setPageTourStatus(pageNames[k], true);
-    });
-
-    $(`#reset-tour-button`).addClass(`disabled`);
-});
