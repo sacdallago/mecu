35ba54457fa0b57a220740b40edcbbc37267d068
Klaus Niedermair <klaus.niedermair@quickbirdstudios.com>
2019-02-11 22:05:37 +0100

    fix(related complexes): only list complexes, which actually also contain this protein

--- app/daos/complexes/getComplexWhichHasProtein.js
+++ app/daos/complexes/getComplexWhichHasProtein.js
@@ -2,8 +2,8 @@ module.exports = {
     query: () => {
         return `
             select c.id, c.name, c.comment, c.proteins
-            from complexes c, proteins p, protein_complexes cp
-            where c.id = cp."complexId" and p."uniprotId" = cp."uniprotId" and p."uniprotId" = :uniprotId;
+            from complexes c
+            where :uniprotId = ANY (c.proteins::text[])
         `;
     }
 };
